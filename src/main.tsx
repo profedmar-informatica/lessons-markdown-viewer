3134b242c2786d3d382e3feb4958bd5cdd156934
[dyad] <profedmar.informatica@gmail.com>
2026-03-01 01:02:15 -0300

    [dyad] Corrigindo erro de caminho de estilo do highlight.js - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+import 'highlight.js/styles/github-light.css'; // Importação do estilo highlight.js movida para cá
 
 createRoot(document.getElementById("root")!).render(<App />);
