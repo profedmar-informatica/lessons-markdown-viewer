96aa033f65478667088e8b979de9bcdc18361869
[dyad] <profedmar.informatica@gmail.com>
2026-03-01 14:00:22 -0300

    [dyad] Corrigido erro de tipo do 'ref' no componente ResizableHandle - wrote 3 file(s)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -19,7 +19,7 @@ const ResizablePanelGroup = ({
 
 const ResizablePanel = Panel;
 
-interface ResizableHandleProps extends React.ComponentPropsWithRef<typeof PanelResizeHandle> {
+interface ResizableHandleProps extends React.ComponentPropsWithoutRef<typeof PanelResizeHandle> {
   withHandle?: boolean;
 }
 
@@ -27,23 +27,25 @@ const ResizableHandle = React.forwardRef<
   React.ElementRef<typeof PanelResizeHandle>,
   ResizableHandleProps
 >(({ className, withHandle, ...props }, ref) => {
-  const PanelHandleComponent = PanelResizeHandle as React.ComponentType<ResizableHandleProps>; // Cast to a component type
+  // Usamos uma asserção de tipo para contornar o problema de inferência do TypeScript
+  // com o 'ref' em componentes aninhados de forwardRef de bibliotecas externas.
+  const PanelResizeHandleComponent = PanelResizeHandle as React.ComponentType<any>;
 
   return (
-    <PanelHandleComponent
-      ref={ref}
+    <PanelResizeHandleComponent
+      ref={ref} // Passa o ref diretamente
       className={cn(
         "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:top-1/2 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:hover:h-[5px] hover:after:bg-border-foreground hover:bg-primary",
         className
       )}
-      {...props}
+      {...props} // Passa as props restantes
     >
       {withHandle && (
         <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
           <GripVertical className="h-2.5 w-2.5" />
         </div>
       )}
-    </PanelHandleComponent>
+    </PanelResizeHandleComponent>
   );
 });
 ResizableHandle.displayName = "ResizableHandle";
