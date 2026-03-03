9f81b207d018891ddf2df344babc89156627b951
[dyad] <profedmar.informatica@gmail.com>
2026-03-03 11:42:53 -0300

    [dyad] Ajustando a cor de fundo da caixa de código para #515151. - wrote 1 file(s)

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -58,7 +58,7 @@ const MarkdownViewer: React.FC<MarkdownViewerProps> = ({ content }) => {
             
             return (
               <div className="relative">
-                <pre className="rounded-lg p-4 pr-12 bg-[#333333] text-[#D4D4D4] font-mono text-[0.99em] leading-relaxed border border-white/5 dark:border-white/10 overflow-x-auto">
+                <pre className="rounded-lg p-4 pr-12 bg-[#515151] text-[#D4D4D4] font-mono text-[0.99em] leading-relaxed border border-white/5 dark:border-white/10 overflow-x-auto">
                   {children}
                 </pre>
                 {codeContent && (
