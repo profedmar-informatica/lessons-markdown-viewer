3314c13661768c4c71e40ee9ddbe6307ff6dc158
[dyad] <profedmar.informatica@gmail.com>
2026-03-11 16:41:13 -0300

    [dyad] Corrigido erro de tipo em ResizableHandleProps usando React.ComponentPropsWithoutRef. - wrote 3 file(s)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -10,23 +10,23 @@ const ResizablePanelGroup = PanelGroup;
 const ResizablePanel = Panel;
 
 // Define as props para o componente ResizableHandle
-// Inclui todas as props de BasePanelResizeHandleProps e adiciona 'withHandle'
-interface ResizableHandleProps extends BasePanelResizeHandleProps {
+// Usa ComponentPropsWithoutRef para garantir que 'ref' não esteja em 'props'
+interface ResizableHandleProps extends React.ComponentPropsWithoutRef<typeof PanelResizeHandle> {
   withHandle?: boolean;
 }
 
 const ResizableHandle = React.forwardRef<
   HTMLDivElement, // Alterado para HTMLDivElement, assumindo que PanelResizeHandle renderiza um div
-  ResizableHandleProps
+  ResizableHandleHandleProps // Tipo das props do componente ResizableHandle
 >(({ className, withHandle, ...props }, ref) => {
   return (
     <PanelResizeHandle
-      ref={ref}
+      ref={ref} // Passa o ref do forwardRef diretamente
       className={cn(
         "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0",
         className
       )}
-      {...props}
+      {...props} // Passa as demais props
     >
       {withHandle && (
         <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
